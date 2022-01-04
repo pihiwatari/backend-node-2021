9915515868a3bb2981cf73f51bbbd439e7ace0d7
erayas <eliasrayasg@gmail.com>
2022-01-03 21:48:11 -0600

    Latest commit

--- app/routes/products.router.js
+++ app/routes/products.router.js
@@ -12,10 +12,7 @@ const service = new ProductsService();
 // y nuestro router solo se encarga de acceder a los servicios.
 
 router.get('/', (req, res) => {
-  // Limitamos
-  const { size } = req.query;
-  const limit = size;
-  const products = service.find(limit);
+  const products = service.find();
   res.status(200).json(products);
 });
 
@@ -33,32 +30,26 @@ router.get('/:productId', (req, res) => {
 router.post('/', (req, res) => {
   // Recibimos el body desde el objeto req.body
   const body = req.body;
-
-  // Respondemos con un JSON que incluya el body y un mensaje.
-  // Agregamos el atributo status con el codigo que queremos responder
-  res.status(201).json({
-    message: 'Created',
-    data: body,
-  });
+  const newPorduct = service.create(body);
+  res.status(201).json(newPorduct);
 });
 
 router.patch('/:productId', (req, res) => {
   const { productId } = req.params;
   const body = req.body;
-
+  const updatedProduct = service.update(productId, body);
   res.status(200).json({
-    productId,
-    message: 'Updated',
-    data: body,
+    message: 'Successfully updated',
+    data: updatedProduct,
   });
 });
 
 router.delete('/:productId', (req, res) => {
   const { productId } = req.params;
-
+  const deletedProduct = service.delete(productId);
   res.status(200).json({
-    productId,
     message: 'Deleted',
+    data: deletedProduct,
   });
 });
 
--- app/services/products.service.js
+++ app/services/products.service.js
@@ -25,15 +25,55 @@ class ProductsService {
     }
   }
 
-  find(limit = 10) {
-    return this.products.slice(0, limit);
+  find() {
+    return this.products;
   }
+
   findOne(id) {
     return this.products.find((product) => product.id === id);
   }
-  create() {}
-  update() {}
-  delete() {}
+
+  create(data) {
+    const newProduct = {
+      id: faker.datatype.uuid(),
+
+      // Con esto copiamos los valores de data, asi como su estructura
+      ...data,
+    };
+
+    this.products.push(newProduct);
+    return newProduct;
+  }
+
+  update(id, changes) {
+    const index = this.products.findIndex((product) => product.id === id);
+    if (index === -1) {
+      throw new Error(`This item doesn't exists`);
+    }
+
+    // Obtenemos el producto original del array y con spread
+    // copiamos los atributos y agregamos o sustituimos los cambios
+    const product = this.products[index];
+    this.products[index] = {
+      ...product,
+      ...changes,
+    };
+    return this.products[index];
+  }
+
+  delete(id) {
+    const index = this.products.findIndex((product) => product.id === id);
+    if (index === -1) {
+      throw new Error(`This item doesn't exists`);
+    }
+
+    // Crea una copia del producto a borrar para retornarla e indicar
+    // cual fue el elemento que se elimino
+
+    const product = this.products[index];
+    this.products.splice(index, 1);
+    return product;
+  }
 }
 
 module.exports = ProductsService;
