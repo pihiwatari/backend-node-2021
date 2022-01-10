e81ad0d6850b2ad0ba85980092a7c791d5872857
Elias Rayas <eliasrayasg@gmail.com>
2022-01-10 09:23:05 -0600

    Added heroku Procfile

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routerApi = require('./routes');
+const cors = require('cors');
 const {
   errorHandler,
   errorLogger,
@@ -7,7 +8,23 @@ const {
 } = require('./middlewares/error.handler');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+// CORS
+const whitelist = ['http://localhotst:5500', 'http://localhost:8080'];
+
+const options = {
+  origin: (origin, callback) => {
+    //Agregamos !origin en caso de que el request provenga del mismo origen
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed'));
+    }
+  },
+};
+
+app.use(cors(options));
 
 // Middleware para recibir objetos json
 
@@ -28,6 +45,4 @@ app.use(errorLogger); // Utiliza next(), entonces ejecuta al siguiente middlewar
 app.use(boomErrorHandler); // Utiliza next(), entonces ejecuta al siguiente middleware
 app.use(errorHandler); // No tiene next(), se ejecuta y no llama al siguiente middleware.
 
-app.listen(PORT, () => {
-  console.log(`Running on port ${PORT}`);
-});
+app.listen(PORT);
